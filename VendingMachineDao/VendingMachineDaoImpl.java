39b2e4736ecb857ed73804194c2f6ddeaca3aa9c
urvaxirani <88558812+urvaxirani@users.noreply.github.com>
2022-02-18 13:04:14 -0500

    Update VendingMachineDaoImpl.java

--- VendingMachineDao/VendingMachineDaoImpl.java
+++ VendingMachineDao/VendingMachineDaoImpl.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package VendingMachineDao;
+package com.sg.vendingmachine.dao;
 
-import VendingMachineDto.Coin;
-import VendingMachineDto.VendingMachineItem;
+import com.sg.vendingmachine.dto.Coin;
+import com.sg.vendingmachine.dto.VendingMachineItem;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
