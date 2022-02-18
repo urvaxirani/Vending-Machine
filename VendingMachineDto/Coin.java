eb5633052a758b40c9c840c4899ba1db34fecce7
urvaxirani <88558812+urvaxirani@users.noreply.github.com>
2022-02-18 13:05:15 -0500

    Update Coin.java

--- VendingMachineDto/Coin.java
+++ VendingMachineDto/Coin.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package VendingMachineDto;
+package com.sg.vendingmachine.dto;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
