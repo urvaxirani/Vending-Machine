6cba8b4a4e18e622100c7945e8c29294eda99fb4
urvaxirani <88558812+urvaxirani@users.noreply.github.com>
2022-02-18 13:00:36 -0500

    Update App.java

--- VendingMachineApp/App.java
+++ VendingMachineApp/App.java
@@ -3,39 +3,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package VendingMachineApp;
+package com.sg.vendingmachine;
 
-import VendingMachineController.VendingMachineController;
-import VendingMachineDao.VendingMachineAuditDao;
-import VendingMachineDao.VendingMachineAuditDaoImpl;
-import VendingMachineDao.VendingMachineDao;
-import VendingMachineDao.VendingMachineDaoImpl;
-import VendingMachineService.VendingMachineServiceLayer;
-import VendingMachineService.VendingMachineServiceLayerImpl;
-import VendingMachineui.UserIO;
-import VendingMachineui.UserIOConsoleImpl;
-import VendingMachineui.VendingMachineView;
+import com.sg.vendingmachine.controller.VendingMachineController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
  * @author urvax
  */
 public class App {
+
     public static void main(String[] args) {
-    
-    UserIO myIo = new UserIOConsoleImpl();
-    // Instantiate the View and wire the UserIO implementation into it
-    VendingMachineView myView = new VendingMachineView(myIo);
-    // Instantiate the DAO
-    VendingMachineDao myDao = new VendingMachineDaoImpl();
-    // Instantiate the Audit DAO
-    VendingMachineAuditDao myAuditDao = new VendingMachineAuditDaoImpl();
-    // Instantiate the Service Layer and wire the DAO and Audit DAO into it
-    VendingMachineServiceLayer myService = new VendingMachineServiceLayerImpl(myDao, myAuditDao);
-    // Instantiate the Controller and wire the Service Layer into it
-    VendingMachineController controller = new VendingMachineController(myService, myView);
-    // Kick off the Controller
-    controller.run();
-   
+
+        //UserIO myIo = new UserIOConsoleImpl();
+        // Instantiate the View and wire the UserIO implementation into it
+        //VendingMachineView myView = new VendingMachineView(myIo);
+        // Instantiate the DAO
+        //VendingMachineDao myDao = new VendingMachineDaoImpl();
+        // Instantiate the Audit DAO
+        //VendingMachineAuditDao myAuditDao = new VendingMachineAuditDaoImpl();
+        // Instantiate the Service Layer and wire the DAO and Audit DAO into it
+        //VendingMachineServiceLayer myService = new VendingMachineServiceLayerImpl(myDao, myAuditDao);
+        // Instantiate the Controller and wire the Service Layer into it
+        //VendingMachineController controller = new VendingMachineController(myService, myView);
+        // Kick off the Controller
+        //controller.run();
+        ApplicationContext ctx
+                = new ClassPathXmlApplicationContext("applicationContext.xml");
+        VendingMachineController controller
+                = ctx.getBean("controller", VendingMachineController.class);
+        controller.run();
     }
 }
