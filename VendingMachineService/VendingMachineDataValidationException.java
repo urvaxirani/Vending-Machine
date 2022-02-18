02171ea505da0b8a579ddd22b1ff67c110772d56
urvaxirani <88558812+urvaxirani@users.noreply.github.com>
2022-02-18 13:07:22 -0500

    Update VendingMachineDataValidationException.java

--- VendingMachineService/VendingMachineDataValidationException.java
+++ VendingMachineService/VendingMachineDataValidationException.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package VendingMachineService;
+package com.sg.vendingmachine.service;
 
 /**
  *
